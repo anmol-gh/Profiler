504cbf18aa4e04c4051ad0a6f79e65067b05aaef
noob-anmol <anmolghai50@gmail.com>
2023-08-04 14:55:52 +0530

    fixed minor css breakpoints

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Header from "./components/Header";
 import Search from "./components/Search";
 import Layout from "./components/Layout";
 import "./App.css";
@@ -6,11 +7,7 @@ import "./App.css";
 const App = () => {
 	return (
 		<div>
-			<h1 className='title'>Profiler</h1>
-			<p className='description'>
-				Check username availability on <br></br>{" "}
-				<span className='description-span'>10+</span> platforms in seconds.
-			</p>
+			<Header/>
 			<Search />
 			<Layout />
 		</div>
