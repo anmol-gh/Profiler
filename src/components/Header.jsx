a48ca7e562a78094ab3c7b07ac9deb90a53d34ac
noob-anmol <anmolghai50@gmail.com>
2023-08-04 14:55:19 +0530

    Separated the header component from App.js

---
+++ src/components/Header.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Typewriter from "typewriter-effect";
+
+const Header = () => {
+	return (
+		<>
+			<h1 className='title'>PROFILER</h1>
+			<p className='description'>
+				Check username availability on <br></br>{" "}
+				<span className='description-span'>
+					<Typewriter
+						options={{ loop: true, delay: 100, deleteSpeed: "natural" }}
+						onInit={(typewriter) => {
+							typewriter
+								.typeString("Instagram")
+								.pauseFor(1000)
+								.deleteAll()
+								.typeString("Fueler")
+								.pauseFor(1000)
+								.deleteAll()
+								.typeString("Github")
+								.pauseFor(1000)
+								.start();
+						}}
+					/>
+				</span>
+			</p>
+		</>
+	);
+};
+
+export default Header;
